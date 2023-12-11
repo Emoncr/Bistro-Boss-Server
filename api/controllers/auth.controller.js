b88b84401a44a3c50a3686bcaf91a84a691941b6
Emoncr <biplobemon 75945@gmail.com>
2023-12-11 10:53:33 +0600

    server and database conneted

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,3 @@
+export const handleGetRoute = async (req, res, next) => {
+  res.send("this is route");
+};
---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import authRouter from "./api/routes/auth.route.js";
+import mongoose from "mongoose";
+import "dotenv/config";
+
+const app = express();
+
+const port = process.env.PORT;
+
+// ======= Connect to DB
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("DB connected successfully");
+  } catch (error) {
+    console.log(error);
+  }
+};
+connectDB();
+// Routes Part
+
+app.use("/", authRouter);
+
+app.listen(port, () => {
+  console.log("app is listing in port 3000");
+});
